45406825d951808a830f2dab7b5de7e258377e6d
Wasiur Rahman <wrahman0@gmail.com>
2014-03-26 22:36:32 -0400

    Initial Commit

---
+++ BrickBreaker/MyScene.h
@@ -0,0 +1,13 @@
+//
+//  MyScene.h
+//  BrickBreaker
+//
+
+//  Copyright (c) 2014 Wasiur Rahman. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface MyScene : SKScene
+
+@end
---
+++ BrickBreaker/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  BrickBreaker
+//
+//  Created by Wasiur Rahman on 2014-03-26.
+//  Copyright (c) 2014 Wasiur Rahman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
